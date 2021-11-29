18df090d979a3c38979b165ba3659f4290ddc12d
Mathias M. Thiim <mathias_thiim@hotmail.com>
2021-11-29 11:06:53 +0100

    Vælg behandling label

--- src/app/booking/book-appointment/book-appointment.component.ts
+++ src/app/booking/book-appointment/book-appointment.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {BookingTimeslotService} from "../shared/booking-timeslot.service";
 import {Observable} from "rxjs";
 import {TimeSlotListDto} from "../shared/time-slot-list.dto";
+import {TreatmentListDto} from "../../treatments/shared/treatmentListDto";
+import {TreatmentsService} from "../../treatments/shared/treatments.service";
 
 @Component({
   selector: 'app-book-appointment',
@@ -11,11 +13,13 @@ import {TimeSlotListDto} from "../shared/time-slot-list.dto";
 
 export class BookAppointmentComponent implements OnInit {
   timeSlots$: Observable<TimeSlotListDto> | undefined;
+  treatmentList$: Observable<TreatmentListDto> | undefined;
 
-  constructor(private _bookingService: BookingTimeslotService) { }
+  constructor(private _bookingService: BookingTimeslotService, private _treatmentsService: TreatmentsService) { }
 
   ngOnInit(): void {
     this.timeSlots$ = this._bookingService.getAll();
+    this.treatmentList$ = this._treatmentsService.getAll();
   }
 }
 
