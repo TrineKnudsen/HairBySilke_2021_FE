280d6fbfcf53ea0bce6510f8da5f9e0c38d2974a
Minaac-ops <72027505+Minaac-ops@users.noreply.github.com>
2021-12-15 10:50:29 +0100

    aboutus

--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -4,16 +4,18 @@ import { CommonModule } from '@angular/common';
 import { AboutRoutingModule } from './about-routing.module';
 import {HttpClientModule} from "@angular/common/http";
 import {AboutComponent} from "./about.component";
+import {MatCardModule} from "@angular/material/card";
 
 
 @NgModule({
   declarations: [
     AboutComponent
   ],
-  imports: [
-    CommonModule,
-    AboutRoutingModule,
-    HttpClientModule
-  ]
+    imports: [
+        CommonModule,
+        AboutRoutingModule,
+        HttpClientModule,
+        MatCardModule
+    ]
 })
 export class AboutModule { }
